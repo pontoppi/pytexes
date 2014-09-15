08daa3a0bdca1cf8fb5197b6de653937e030006e
Klaus Pontoppidan <pontoppi@stsci.edu>
2014-09-15 15:17:46 -0400

    creates pytexes subfolder and moved scripts

---
+++ pytexes/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='pytexes',
+      version='0.1.0',
+      description='TEXES reduction library',
+      author='Klaus Pontoppidan',
+      author_email='pontoppi@stsci.edu',
+      url='http://www.stsci.edu/~pontoppi',
+      packages=['pytexes'],
+      package_data={'pytexes': ['*.ini']}
+      )
+
+    
